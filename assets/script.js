e920bc5bdbed64d8e42551a3a0eefee1a47a14c8
Vince Momot <vincemomot@gmail.com>
2021-10-06 10:53:24 -0400

    dropdown working, readME started and text input created

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
-button.addEventListener("click",show)
-dropDownEL=document.querySelector("dropdown-menu");
-function show(){
-    dropDownEL.setAttribute( 'class', 'myCssClass' );
+var shw = 0;
+
+button.addEventListener("click", show)
+function show() {
+    console.log("here");
+    if (shw == 0) {
+        showMe.style.display = "block";
+        shw++;
+    }
+    else {
+        showMe.style.display = "none";
+        shw--;
+    }
 }
